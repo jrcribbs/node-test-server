7d7636aab9a3b32bafff8953f80655af8fb2dd5f
JR Cribbs <jrd.cribbs@gmail.com>
2022-09-13 09:49:26 -0400

    inital commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+
+app.get('/hello', (req, res) =>
+    res.send('Hello World!'));
+
+const PORT = 4000;
+app.listen(PORT);
+// load the express library
+// create an instance of the library
+
+// use express library to listen for URL pattern "/hello"
+// respond with string "Hello World!"
+
+
+// listen to port 4000
+
